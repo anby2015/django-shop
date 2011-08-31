147dd2e6e566eda69074ef06bc9c6eb11d57b231
root <root@foo.localdomain>
2011-09-01 01:35:10 +1100

    s/\s/    /g

--- shop/users/forms.py
+++ shop/users/forms.py
@@ -6,12 +6,12 @@ from django.utils.translation import ugettext_lazy as _
 from users.models import Profile
 
 class RegisterUserForm(UserCreationForm):
-	username = RegexField(label=_("Username"), max_length=30, regex=r'^[\w]+$',
-		help_text = _("Required. 30 characters or fewer. Letters, digits and _ only."),
-		error_messages = {'invalid': _("This value may contain only letters, numbers and _ characters.")},
-	)
-	email = EmailField(label=_("Please, enter your e-mail for verification"), required=not DEBUG)
-	error_css_class = 'error'
+    username = RegexField(label=_("Username"), max_length=30, regex=r'^[\w]+$',
+        help_text = _("Required. 30 characters or fewer. Letters, digits and _ only."),
+        error_messages = {'invalid': _("This value may contain only letters, numbers and _ characters.")},
+    )
+    email = EmailField(label=_("Please, enter your e-mail for verification"), required=not DEBUG)
+error_css_class = 'error'
 
 class ProfileForm(ModelForm):
     class Meta:
