fd403bee78badb6d56acd67d41421d5dfa253558
root <root@foo.localdomain>
2011-09-01 01:19:49 +1100

    profile model with <s>blackjack</s>cakes and turkey

---
+++ shop/users/fields.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from users.countries import COUNTRIES
+
+#adapted from http://www.djangosnippets.org/snippets/494/
+#and http://djangosnippets.org/snippets/1281/
+#using UN country and 3 char code list
+#from http://unstats.un.org/unsd/methods/m49/m49alpha.htm
+
+
+class CountryField(models.CharField):
+    def __init(self, *args, **kwargs):
+        kwargs.setdefault('maxlength', 3)
+        kwargs.setdefault('choices', COUNTRIES)
+
+        super(CountryField, self).__init__(*args, **kwargs)
+
+    def get_internal_type(self):
+        return "CharField"
--- shop/users/models.py
+++ shop/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models import IntegerField, DateField, CharField, OneToOneField
+from django.contrib.auth.models import User
 
-# Create your models here.
+from users.fields import CountryField
+
+GENDER_CHOICES = (
+    (0, 'Not sure'),
+    (1, 'Male'),
+    (2, 'Female'),
+)
+class Profile(User):
+    # p.user is much trettier then p.user_ptr
+    user = OneToOneField(User, parent_link=True)
+    
+    gender = IntegerField(choices=GENDER_CHOICES, default=0)
+    birthday = DateField()
+    country = CountryField()
+    city = CharField(max_length=200)
