88ad41c10fad412046ca64812b2ad784590718a2
root <root@foo.localdomain>
2011-09-01 01:18:19 +1100

    authentications views updated with new class decorators, logout view has also became class-based

--- shop/users/urls.py
+++ shop/users/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
-from users.views import Login
+
+import users.views
 
 urlpatterns = patterns('users.views',
-    url(r'^login/$', Login.as_view()),
-    url(r'^logout/$', 'logout'),
-    
+    url(r'^login/$', users.views.Login.as_view()),
+    url(r'^logout/$', users.views.Logout.as_view()),
+    url(r'^create/$', users.views.Register.as_view()),
 )
--- shop/users/views.py
+++ shop/users/views.py
@@ -1,12 +1,15 @@
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import never_cache
-from django.views.decorators.csrf import csrf_protect
-from django.views.generic.edit import FormView
+from django.views.generic.base import View, TemplateResponseMixin
+from django.views.generic.edit import FormView, BaseCreateView
 from django.contrib.auth import login, logout as auth_logout, REDIRECT_FIELD_NAME as AUTH_REDIRECT_FIELD_NAME
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponseRedirect
 from django.conf import settings
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_http_methods
+
+from main.class_decorators import login_required, csrf_protect, never_cache, unauthorized_only
+from users.forms import RegisterUserForm
+
+require_GET_POST = require_http_methods(['GET', 'POST'])
 
 if not hasattr(settings, 'REDIRECT_FIELD_NAME'):
     setattr(settings, 'REDIRECT_FIELD_NAME', AUTH_REDIRECT_FIELD_NAME)
@@ -23,31 +26,30 @@ def get_redirect_url(request, redirect_field_name=settings.REDIRECT_FIELD_NAME):
         redir_to = settings.LOGIN_REDIRECT_URL
     return redir_to
 
+@csrf_protect
+@never_cache
+@unauthorized_only
 class LoginView(FormView):
     """
     A bit generic class-based login view
     appears as rework of django.contirb.auth.views.login
+    customized for own needs
     """
-    
+
     form_class = AuthenticationForm
     redirect_field_name = settings.REDIRECT_FIELD_NAME
     template_name = 'users/login.html'
 
-    @method_decorator(csrf_protect)
-    @method_decorator(never_cache)
-    def dispatch(self, *args, **kwargs):
-        return super(LoginView, self).dispatch(*args, **kwargs) 
-
     def form_valid(self, form):
         login(self.request, form.get_user())
         return super(LoginView
-    
+
     def get_redirect_url(self):
         return get_redirect_url(self.request, self.redirect_field_name)
 
     def get_success_url(self):
         return self.success_url or self.get_redirect_url()
-            
+    
     def get_context_data(self, **kwargs):
         data = {
             'redirect_field_name': self.redirect_field_name,
@@ -56,9 +58,9 @@ class LoginView(FormView):
         data.update(kwargs)
         return data
 
-@require_POST
 @login_required
-def logout(request):
-    auth_logout(request.user)
-    return HttpResponseRedirect(get_redirect_url(request))
+class Logout(View):
+    def post(request):
+        auth_logout(request.user)
+        return HttpResponseRedirect(get_redirect_url(request))
 
