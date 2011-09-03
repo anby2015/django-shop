931f9ba2530673adb67a13c88c7d9f1c485b01ae
root <root@foo.localdomain>
2011-09-04 05:16:38 +1100

    added products and categories models

---
+++ shop/goods/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.db.models import Model, IntegerField, DateField, CharField, TextField, ForeignKey
+
+class Category(Model):
+    name = CharField(max_length=50)
+    inheritance = TextField() # /\d+(\.\d+)*/
+    
+    def set_parent(self, c, save=False):
+        self.inheritance = '%s.%d' % (c.inheritance, c.pk)
+        self.save(commit=save)
+    
+    def extract_parent(self, c, queryset=None, do_get=True):
+        q = queryset or Category.objects.all()
+        params = {'pk': self.inheritance.split('.')[-1]}
+        return q.get(**params) if do_get else q.filter(**params)[0]
+    
+    def get_parents_line(self, queryset=None):
+        q = queryset or Category.objects.all()
+        return q.filter(pk__in=inheritance.split('.'))
+
+class Product(Model):
+    name = CharField(max_length=200)
+    description = TextField()
+    category = ForeignKey(Category)
--- shop/settings.py
+++ shop/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'main',
     'users',
+    'goods',
 )
 
 # A sample logging configuration. The only tangible logging
