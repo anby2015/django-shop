d7074dd093732632f6d6d53918814aa97d50c88d
root <root@foo.localdomain>
2011-09-05 04:56:37 +1100

    added adminka

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+
 from users.views import CompleteRegistration
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,10 +11,10 @@ urlpatterns = patterns('',
     # url(r'^shop/', include('shop.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('main.urls')),
     url(r'^users/', include('users.urls')),
     url(r'^thanks/', CompleteRegistration.as_view()),
