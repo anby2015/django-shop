331097cb59a19907cb06c85faaa0eca23e346fdd
foobarrior <nikitamalyavin@gmail.com>
2011-09-09 03:14:55 +1100

    main page view implementation

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from djnago.shortcuts import render
+
+from goods.models import Category, Product, get_category_roots
+
+PAGINATE_BY = 50
+
+def get_page(q, page_num, from_one=True):
+    return q[PAGINATE_BY * (page_num - (1 if from_one else 0)) : PAGINATE_BY]
+
+def index(request, page=1):
+    context = {
+        'in_root': True,
+        'categories': get_category_roots(),
+        'latest_products': get_page(Product.objects.order_by('-date'), page)
+    }
+    return render(request, 'index.html', context)
