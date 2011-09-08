94d5674651673f134645b3d2153b2da3edbfaede
foobarrior <nikitamalyavin@gmail.com>
2011-09-09 02:00:05 +1100

    reworked `Category` inheritance model

--- shop/goods/models.py
+++ shop/goods/models.py
@@ -3,11 +3,20 @@ from django.db.models import Model, IntegerField, DateField, CharField, TextFiel
 
 class Category(Model):
     name = CharField(max_length=50)
-    inheritance = TextField() # /\d+(\.\d+)*/
+    
+    # validate `inheritance` as /|\d+(\.\d+)*/
+    # if it is empty, then the category is root
+    # it doesn't contain itself
+    inheritance = TextField()
+    
+    def as_parent(self):
+        inheritance, pk = self.inheritance, self.pk
+        return '%s.%d' % (inheritance, pk) if inheritance else str(pk)
     
     def set_parent(self, c, save=False):
-        self.inheritance = '%s.%d' % (c.inheritance, c.pk)
-        self.save(commit=save)
+        self.inheritance = c.as_parent()
+        if save:
+            self.save()
     
     def extract_parent(self, c, queryset=None, do_get=True):
         q = queryset or Category.objects.all()
@@ -20,10 +29,14 @@ class Category(Model):
     
     def get_dhildren(self, queryset=None):
         q = queryset or Category.objects.all()
-        return q.filter(inheritance__regexp=self.inheritance + r'\.\d+')
+        return q.filter(inheritance=self.as_parent())
     
 
 class Product(Model):
     name = CharField(max_length=200)
     description = TextField()
     category = ForeignKey(Category)
+    
+
+def get_category_roots():
+    return Category.objects.filter(inheritance='')
